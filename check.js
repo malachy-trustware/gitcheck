f6e6ed0edb46abf619ea0b0ee5a7a385c58fb08d
Malachy Olua <oluamalachy95@gmail.com>
2023-05-19 01:00:06 +0100

    check commit

---
+++ check.js
@@ -0,0 +1,7 @@
+// a javascript function
+function check() {
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+    var c = document.getElementById("c").value;
+
+}
